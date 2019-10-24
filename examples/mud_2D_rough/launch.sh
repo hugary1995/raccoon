7e365143141a01fde3c1905c4c76556c291f5cc2
Gary Hu <th224@duke.edu>
2019-10-24 10:42:27 -0400

    add launch script

---
+++ examples/mud_2D_rough/launch.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+cd run_1/0 && sbatch run1_0
+cd run_1/0.25 && sbatch run1_0.25
+cd run_1/0.5 && sbatch run1_0.5
+cd run_1/0.75 && sbatch run1_0.75
+cd run_1/1 && sbatch run1_1
+
+cd run_2/0 && sbatch run2_0
+cd run_2/0.25 && sbatch run2_0.25
+cd run_2/0.5 && sbatch run2_0.5
+cd run_2/0.75 && sbatch run2_0.75
+cd run_2/1 && sbatch run2_1
+
+cd run_3/0 && sbatch run3_0
+cd run_3/0.25 && sbatch run3_0.25
+cd run_3/0.5 && sbatch run3_0.5
+cd run_3/0.75 && sbatch run3_0.75
+cd run_3/1 && sbatch run3_1
+
+cd run_4/0 && sbatch run4_0
+cd run_4/0.25 && sbatch run4_0.25
+cd run_4/0.5 && sbatch run4_0.5
+cd run_4/0.75 && sbatch run4_0.75
+cd run_4/1 && sbatch run4_1
+
+cd run_5/0 && sbatch run5_0
+cd run_5/0.25 && sbatch run5_0.25
+cd run_5/0.5 && sbatch run5_0.5
+cd run_5/0.75 && sbatch run5_0.75
+cd run_5/1 && sbatch run5_1
+
+cd run_6/0 && sbatch run6_0
+cd run_6/0.25 && sbatch run6_0.25
+cd run_6/0.5 && sbatch run6_0.5
+cd run_6/0.75 && sbatch run6_0.75
+cd run_6/1 && sbatch run6_1
+
+cd run_7/0 && sbatch run7_0
+cd run_7/0.25 && sbatch run7_0.25
+cd run_7/0.5 && sbatch run7_0.5
+cd run_7/0.75 && sbatch run7_0.75
+cd run_7/1 && sbatch run7_1
+
+cd run_8/0 && sbatch run8_0
+cd run_8/0.25 && sbatch run8_0.25
+cd run_8/0.5 && sbatch run8_0.5
+cd run_8/0.75 && sbatch run8_0.75
+cd run_8/1 && sbatch run8_1
+
+cd run_9/0 && sbatch run9_0
+cd run_9/0.25 && sbatch run9_0.25
+cd run_9/0.5 && sbatch run9_0.5
+cd run_9/0.75 && sbatch run9_0.75
+cd run_9/1 && sbatch run9_1
+
+cd run_10/0 && sbatch run10_0
+cd run_10/0.25 && sbatch run10_0.25
+cd run_10/0.5 && sbatch run10_0.5
+cd run_10/0.75 && sbatch run10_0.75
+cd run_10/1 && sbatch run10_1
