74ad3b1252ae9b0917fa9f476d9b3d42d17e580c
Bo Zeng <bz75@dcc-login-01.oit.duke.edu>
2021-09-02 21:33:02 -0400

    hand clang format src 2 #92

--- src/auxkernels/ConditionalBoundsAux.C
+++ src/auxkernels/ConditionalBoundsAux.C
@@ -13,10 +13,9 @@ ConditionalBoundsAux::validParams()
       "value"
       "for kumar's phase-field fracture model 2020"
       "variable to PETSc's SNES variational inequalities solver.");
-  params.addRequiredParam<Real>(
-      "fixed_bound_value", "The value of fixed bound for the variable");
-  params.addRequiredParam<Real>(
-      "threshold_value", "The threshold for conditional history bound for the variable");
+  params.addRequiredParam<Real>("fixed_bound_value", "The value of fixed bound for the variable");
+  params.addRequiredParam<Real>("threshold_value", "The threshold for conditional history bound for the variable");
+  
   return params;
 }
 
--- src/materials/GeneralizedExternalDrivingForce.C
+++ src/materials/GeneralizedExternalDrivingForce.C
@@ -34,11 +34,11 @@ GeneralizedExternalDrivingForce::validParams()
   params.addRequiredParam<Real>("delta", "delta");
   params.addParam<MaterialPropertyName>("rank_two_tensor", "stress", "name of the stress tensor");
   params.addParam<MaterialPropertyName>("external_driving_force_name", "ex_driving", "name of the material that holds the external_driving_force");
+
   return params;
 }
 
-GeneralizedExternalDrivingForce::GeneralizedExternalDrivingForce(
-    const InputParameters & parameters)
+GeneralizedExternalDrivingForce::GeneralizedExternalDrivingForce(const InputParameters & parameters)
   : Material(parameters),
     _ex_driving_name(getParam<MaterialPropertyName>("external_driving_force_name")),
     _ex_driving(declareADProperty<Real>(_ex_driving_name)),
@@ -58,30 +58,23 @@ void
 GeneralizedExternalDrivingForce::computeQpProperties()
 {
   ADReal K = _Lambda[_qp] + 2.0 * _mu[_qp] / 3.0;
-  ADReal _gamma_0 =
-        (_mu[_qp] + 3 * K) * _sigma_ts * _L[_qp] / _Gc[_qp] / 18 / _mu[_qp] / _mu[_qp] / K / K;
+  ADReal _gamma_0 = (_mu[_qp] + 3 * K) * _sigma_ts * _L[_qp] / _Gc[_qp] / 18 / _mu[_qp] / _mu[_qp] / K / K;
   ADReal _gamma_1 = (1.0 + _delta) / (2.0 * _sigma_ts * _sigma_cs);
-  ADReal _gamma_2 =
-        (8 * _mu[_qp] + 24 * K - 27 * _sigma_ts) / 144 / _mu[_qp] / K;
+  ADReal _gamma_2 = (8 * _mu[_qp] + 24 * K - 27 * _sigma_ts) / 144 / _mu[_qp] / K;
   ADReal _temp = _Gc[_qp] * 3.0 / _L[_qp] / 8.0;
   ADReal I1 = _stress[_qp](0, 0) + _stress[_qp](1, 1) + _stress[_qp](2, 2);
   ADReal J2 = (pow(_stress[_qp](0, 0) - _stress[_qp](1, 1), 2) +
                pow(_stress[_qp](1, 1) - _stress[_qp](2, 2), 2) +
-               pow(_stress[_qp](0, 0) - _stress[_qp](2, 2), 2)) /
-                  6.0 +
-                  pow(_stress[_qp](0, 1), 2) + pow(_stress[_qp](0, 2), 2) +
-                  pow(_stress[_qp](2, 1), 2);
+               pow(_stress[_qp](0, 0) - _stress[_qp](2, 2), 2)) / 6.0 +
+               pow(_stress[_qp](0, 1), 2) + pow(_stress[_qp](0, 2), 2) +
+               pow(_stress[_qp](2, 1), 2);
 
   mooseAssert(J2 >= 0, "Negative J2");
 
   ADReal _beta_0 = _delta * _temp;
   ADReal _beta_1 = (-_gamma_1 * _temp - _gamma_2) * (_sigma_cs - _sigma_ts) -
                    _gamma_0 * (pow(_sigma_cs, 3) - pow(_sigma_ts, 3));
-  ADReal _beta_2 =
-        std::sqrt(3.0) * ((-_gamma_1 * _temp + _gamma_2) * (_sigma_cs + _sigma_ts) +
-        _gamma_0 * (pow(_sigma_cs, 3) + pow(_sigma_ts, 3)));
+  ADReal _beta_2 = std::sqrt(3.0) * ((-_gamma_1 * _temp + _gamma_2) * (_sigma_cs + _sigma_ts) + _gamma_0 * (pow(_sigma_cs, 3) + pow(_sigma_ts, 3)));
   ADReal _beta_3 = _L[_qp] * _sigma_ts / _mu[_qp] / K / _Gc[_qp];
-
-  _ex_driving[_qp] =
-        (_beta_2 * std::sqrt(J2) + _beta_1 * I1 + _beta_0) / (1 + _beta_3 * I1 * I1);
+  _ex_driving[_qp] = (_beta_2 * std::sqrt(J2) + _beta_1 * I1 + _beta_0) / (1 + _beta_3 * I1 * I1);
 }
