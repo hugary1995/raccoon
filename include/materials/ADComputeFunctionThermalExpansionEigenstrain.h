143747a33f7122d17547ffe49a5656b5f3df0774
Bo Zeng <bo.zeng@duke.edu>
2022-05-09 12:43:51 -0400

    FunctionalThermalExpanEigenstrain

---
+++ include/materials/ADComputeFunctionThermalExpansionEigenstrain.h
@@ -0,0 +1,29 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+#include "Material.h"
+#include "ADComputeThermalExpansionEigenstrainBase.h"
+
+/**
+ * ADComputeFunctionThermalExpansionEigenstrain computes an eigenstrain for thermal expansion
+ * with a functional(material) expansion coefficient.
+ */
+class ADComputeFunctionThermalExpansionEigenstrain : public ADComputeThermalExpansionEigenstrainBase
+{
+public:
+  static InputParameters validParams();
+
+  ADComputeFunctionThermalExpansionEigenstrain(const InputParameters & parameters);
+
+protected:
+  virtual void computeThermalStrain(ADReal & thermal_strain) override;
+
+  const ADMaterialProperty<Real> & _thermal_expansion_coeff;
+};
