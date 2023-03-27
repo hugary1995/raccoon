f3c53dc9ab374196ce11a08f714ccb4577640d76
BoZeng1997 <59585661+BoZeng1997@users.noreply.github.com>
2023-03-27 17:03:37 -0400

    Nucleation model 2020 for heterogeneous material  (#146)
    * allow heterogeneous material for nucleation model 2020
    
    * update tutorial and test for heterogeneous material nucleation model
    
    * add description related to stress balance equation. related issue #145
    
    * correct stress balance equation format #146

--- src/materials/NucleationMicroForce.C
+++ src/materials/NucleationMicroForce.C
@@ -27,18 +27,25 @@ NucleationMicroForce::validParams()
   params.addParam<MaterialPropertyName>("lambda", "lambda", "Lame's first parameter lambda");
   params.addParam<MaterialPropertyName>("shear_modulus", "G", "shear modulus mu or G");
 
-  params.addRequiredParam<Real>(
+  params.addRequiredParam<MaterialPropertyName>(
       "tensile_strength", "The tensile strength of the material beyond which the material fails.");
-  params.addRequiredParam<Real>(
+
+  params.addRequiredParam<MaterialPropertyName>(
       "compressive_strength",
       "The compressive strength of the material beyond which the material fails.");
 
-  params.addRequiredParam<Real>("delta", "delta");
+  params.addRequiredParam<MaterialPropertyName>("delta", "delta");
   params.addParam<MaterialPropertyName>(
       "external_driving_force_name",
       "ex_driving",
       "Name of the material that holds the external_driving_force");
-
+  params.addParam<MaterialPropertyName>(
+      "stress_balance_name",
+      "stress_balance",
+      "Name of the stress balance function $F= \\dfrac{J_2}{\\mu} + \\dfrac{I_1^2}{9\\kappa} - c_e "
+      "-\\dfrac{3\\Gc}{8\\delta}=0 $. This value tells how close the material is to stress "
+      "surface.");
+  params.addParam<MaterialPropertyName>("stress_name", "stress", "Name of the stress tensor");
   return params;
 }
 
@@ -51,10 +58,11 @@ NucleationMicroForce::NucleationMicroForce(const InputParameters & parameters)
     _L(getADMaterialProperty<Real>(prependBaseName("regularization_length", true))),
     _lambda(getADMaterialProperty<Real>(prependBaseName("lambda", true))),
     _mu(getADMaterialProperty<Real>(prependBaseName("shear_modulus", true))),
-    _sigma_ts(getParam<Real>("tensile_strength")),
-    _sigma_cs(getParam<Real>("compressive_strength")),
-    _delta(getParam<Real>("delta")),
-    _stress(getADMaterialProperty<RankTwoTensor>(prependBaseName("stress")))
+    _sigma_ts(getADMaterialProperty<Real>(prependBaseName("tensile_strength", true))),
+    _sigma_cs(getADMaterialProperty<Real>(prependBaseName("compressive_strength", true))),
+    _delta(getADMaterialProperty<Real>(prependBaseName("delta", true))),
+    _stress(getADMaterialProperty<RankTwoTensor>(prependBaseName("stress_name", true))),
+    _stress_balance(declareADProperty<Real>(prependBaseName("stress_balance_name", true)))
 {
 }
 
@@ -66,9 +74,9 @@ NucleationMicroForce::computeQpProperties()
 
   // Parameters in the strength surface
   ADReal gamma_0 =
-      (_mu[_qp] + 3 * K) * _sigma_ts * _L[_qp] / _Gc[_qp] / 18 / _mu[_qp] / _mu[_qp] / K / K;
-  ADReal gamma_1 = (1.0 + _delta) / (2.0 * _sigma_ts * _sigma_cs);
-  ADReal gamma_2 = (8 * _mu[_qp] + 24 * K - 27 * _sigma_ts) / 144 / _mu[_qp] / K;
+      (_mu[_qp] + 3 * K) * _sigma_ts[_qp] * _L[_qp] / _Gc[_qp] / 18 / _mu[_qp] / _mu[_qp] / K / K;
+  ADReal gamma_1 = (1.0 + _delta[_qp]) / (2.0 * _sigma_ts[_qp] * _sigma_cs[_qp]);
+  ADReal gamma_2 = (8 * _mu[_qp] + 24 * K - 27 * _sigma_ts[_qp]) / 144 / _mu[_qp] / K;
 
   // The mobility
   ADReal M = _Gc[_qp] / _L[_qp] / _c0[_qp];
@@ -86,11 +94,12 @@ NucleationMicroForce::computeQpProperties()
     J2.value() = libMesh::TOLERANCE * libMesh::TOLERANCE;
 
   // Compute the external driving force required to recover the desired strength envelope.
-  ADReal beta_0 = _delta * M;
-  ADReal beta_1 = (-gamma_1 * M - gamma_2) * (_sigma_cs - _sigma_ts) -
-                  gamma_0 * (pow(_sigma_cs, 3) - pow(_sigma_ts, 3));
-  ADReal beta_2 = std::sqrt(3.0) * ((-gamma_1 * M + gamma_2) * (_sigma_cs + _sigma_ts) +
-                                    gamma_0 * (pow(_sigma_cs, 3) + pow(_sigma_ts, 3)));
-  ADReal beta_3 = _L[_qp] * _sigma_ts / _mu[_qp] / K / _Gc[_qp];
+  ADReal beta_0 = _delta[_qp] * M;
+  ADReal beta_1 = (-gamma_1 * M - gamma_2) * (_sigma_cs[_qp] - _sigma_ts[_qp]) -
+                  gamma_0 * (pow(_sigma_cs[_qp], 3) - pow(_sigma_ts[_qp], 3));
+  ADReal beta_2 = std::sqrt(3.0) * ((-gamma_1 * M + gamma_2) * (_sigma_cs[_qp] + _sigma_ts[_qp]) +
+                                    gamma_0 * (pow(_sigma_cs[_qp], 3) + pow(_sigma_ts[_qp], 3)));
+  ADReal beta_3 = _L[_qp] * _sigma_ts[_qp] / _mu[_qp] / K / _Gc[_qp];
   _ex_driving[_qp] = (beta_2 * std::sqrt(J2) + beta_1 * I1 + beta_0) / (1 + beta_3 * I1 * I1);
+  _stress_balance[_qp] = J2 / _mu[_qp] + pow(I1, 2) / 9.0 / K - _ex_driving[_qp] - M;
 }
